41adb33558e8cdb276d9346b962c2436923c76cc
Fatezhang <762344938@qq.com>
2019-04-18 22:50:06 +0800

    插入排序提交

--- Algorithm/src/main/java/Alogrithm/sort/InsertSort.java
+++ Algorithm/src/main/java/Alogrithm/sort/InsertSort.java
@@ -9,7 +9,15 @@ import com.alibaba.fastjson.JSON;
 public class InsertSort {
     public static void main(String[] args) {
         int[] arr = {3, 5, 6, 2, 7, 4, 8, 9, 1};
-
+        for (int i = 0; i < arr.length - 1; i++) {
+            int end = i;
+            int tmp = arr[i + 1];
+            while (end >= 0 && arr[end] > tmp) {
+                arr[end + 1] = arr[end];
+                end--;
+            }
+            arr[end + 1] = tmp;
+        }
         System.out.println(JSON.toJSONString(arr));
     }
 }
