36fb4addf8ea3f1818aac6897d23d309ca23324d
Fatezhang <762344938@qq.com>
2019-04-21 17:39:57 +0800

    算法 李白打酒 提交

---
+++ Algorithm/src/main/java/Alogrithm/Alogrithm/liBaiDajiu/Solution.java
@@ -0,0 +1,51 @@
+package Alogrithm.Alogrithm.liBaiDajiu;
+
+/**
+ * 李白大酒 题目解法
+ */
+public class Solution {
+
+    /**
+     * 使用二进制数字进行解题
+     */
+    public static void printWithNum() {
+        // 100000000000000  -  111111111100001
+        for (int i = 16384; i < 32737; i++) {
+            int sum = 2;// 开始有2斗酒
+            int flower = 0;
+            int store = 0;
+            int tmpI = i;
+            int j = 0;
+            for (; j < 15; j++) {
+                // 1表示花 0表示店 遇店加一倍 遇花喝一斗
+                if (tmpI % 2 == 1) {
+                    flower++;
+                    sum -= 1;
+                } else {
+                    store++;
+                    sum *= 2;
+                }
+                if (flower > 10 || sum < 0 || store > 5) {
+                    break;
+                }
+                tmpI /= 2;
+            }
+            if (j == 15 && sum == 0) {
+                tmpI = i;
+                while (j-- != 0) {
+                    if (tmpI % 2 == 1) {
+                        System.out.print("a");
+                    } else {
+                        System.out.print("b");
+                    }
+                    tmpI /= 2;
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printWithNum();
+    }
+}
--- Algorithm/src/main/java/Alogrithm/Alogrithm/palindromeNumber/Solution.java
+++ Algorithm/src/main/java/Alogrithm/Alogrithm/palindromeNumber/Solution.java
@@ -2,14 +2,14 @@ package Alogrithm.Alogrithm.palindromeNumber;
 
 public class Solution {
     public boolean isPalindrome(int x) {
-        if(x >= 0){
+        if (x >= 0) {
             int t = x;
             int f = 0;
-            while(t > 0){
-                f = f*10 + (t - t/10*10);
-                t/=10;
+            while (t > 0) {
+                f = f * 10 + t % 10;
+                t /= 10;
             }
-            if((f ^ x) == 0){
+            if ((f ^ x) == 0) {
                 return true;
             }
         }
