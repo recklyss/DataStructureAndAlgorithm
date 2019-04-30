4a8e279448415eaf74ef73db0683953cda70704a
zhangjiaheng <zhangjiaheng@jianbing.com>
2019-04-30 16:42:14 +0800

    二叉树 前中后序遍历以及按层遍历 提交

--- src/main/java/DataStructure/Tree/TreeOperation.java
+++ src/main/java/DataStructure/Tree/TreeOperation.java
@@ -1,5 +1,7 @@
 package DataStructure.Tree;
 
+import java.util.LinkedList;
+
 /**
  * @Author zhangjiaheng
  * @Description 一些创建二叉树、遍历等方法
@@ -35,9 +37,71 @@ public class TreeOperation {
         return null;
     }
 
+    /**
+     * 递归先序遍历二叉树 左-根-右
+     */
+    public static void frontPrintOutTree(TreeNode root){
+        if(null != root){
+            System.out.print(root.val);
+            frontPrintOutTree(root.left);
+            frontPrintOutTree(root.right);
+        }
+    }
+
+    /**
+     * 递归中序遍历二叉树 左-根-右
+     */
+    public static void midPrintOutTree(TreeNode root){
+        if(null != root){
+            midPrintOutTree(root.left);
+            System.out.print(root.val);
+            midPrintOutTree(root.right);
+        }
+    }
+
+    /**
+     * 递归先序遍历二叉树 左-根-右
+     */
+    public static void lastPrintOutTree(TreeNode root){
+        if(null != root){
+            lastPrintOutTree(root.left);
+            lastPrintOutTree(root.right);
+            System.out.print(root.val);
+        }
+    }
+
+    /**
+     * 按层遍历二叉树
+     */
+    public static void levelPrintOutTree(TreeNode root){
+        if(null != root){
+            LinkedList<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            TreeNode i = root;
+            while(!queue.isEmpty()){
+                if(i.left != null){
+                    queue.add(i.left);
+                }
+                if(i.right != null){
+                    queue.add(i.right);
+                }
+                System.out.print(i.val);
+                queue.poll();
+                i = queue.peek();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] a = {3, 2, 5, 8, 4, 7, 6, 9, 1};
         TreeNode root = createSearchTree(a);
         System.out.println(root);
+        frontPrintOutTree(root);
+        System.out.println();
+        midPrintOutTree(root);
+        System.out.println();
+        lastPrintOutTree(root);
+        System.out.println();
+        levelPrintOutTree(root);
     }
 }
