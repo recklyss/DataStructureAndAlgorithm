850c018161c196dcc4fca8ae63c9a795f795d407
zhangjiaheng <zhangjiaheng@jianbing.com>
2019-05-10 15:46:53 +0800

    去掉main

--- src/main/java/DataStructure/Tree/TreeNode.java
+++ src/main/java/DataStructure/Tree/TreeNode.java
@@ -25,13 +25,4 @@ public class TreeNode {
     public String toString() {
         return "(" + val + ")[left:" + left + ",right:" + right + "]";
     }
-
-    public static void main(String[] args) {
-        TreeNode t2 = new TreeNode(2);
-        TreeNode t3 = new TreeNode(3);
-        TreeNode t1 = new TreeNode(1);
-        t1.left = t2;
-        t1.right = t3;
-        System.out.println(t1);
-    }
 }
