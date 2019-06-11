b96348805202535e00e99a1b570128698d39776d
Fatezhang <762344938@qq.com>
2019-06-11 14:20:50 +0800

    二叉树的非递归后序遍历提交

--- src/main/java/DataStructure/Tree/TreeOperation.java
+++ src/main/java/DataStructure/Tree/TreeOperation.java
@@ -100,6 +100,7 @@ public class TreeOperation {
 
     /**
      * 递归中序遍历二叉树 左-根-右
+     * 二叉搜索树的中序遍历输出的为有序序列
      */
     public static void midPrintOutTree(TreeNode root) {
         if (null != root) {
@@ -111,6 +112,7 @@ public class TreeOperation {
 
     /**
      * 非递归中序遍历
+     * 二叉搜索树的中序遍历输出的为有序序列
      * 思路：使用栈，先将树的左子女入栈
      * 然后在出栈时，每出栈一个元素
      * 判断其是否有右子树，有的话入栈
@@ -126,9 +128,9 @@ public class TreeOperation {
             }
             while (!stack.isEmpty()) {
                 node = stack.pop();
-                if(null != node){
+                if (null != node) {
                     TreeNode n = node.right;
-                    while (n != null){
+                    while (n != null) {
                         stack.push(n);
                         n = n.left;
                     }
@@ -151,6 +153,67 @@ public class TreeOperation {
     }
 
     /**
+     * 非递归后序遍历
+     * 这里采用了逆转先序遍历的方式完成
+     * 先序遍历是 左右根
+     * 后序遍历是 根左右
+     * 这样写的好处是代码简单好理解 缺点是多用了一个栈 时间和空间复杂度增加
+     */
+    public static void lastPrintByOtherStack(TreeNode root) {
+        if (root != null) {
+            final Stack<TreeNode> stack = new Stack<>();
+            final Stack<TreeNode> stackReverse = new Stack<>();
+            stack.push(root);
+            while (!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                if (node != null) {
+                    stackReverse.push(node);
+                    if (node.left != null) {
+                        stack.push(node.left);
+                    }
+                    if (node.right != null) {
+                        stack.push(node.right);
+                    }
+                }
+            }
+            while (!stackReverse.isEmpty()){
+                TreeNode node = stackReverse.pop();
+                System.out.print(node.val + " | ");
+            }
+        }
+    }
+
+    /**
+     * 非递归后序遍历二叉树
+     * 解法二
+     * 相较于上面那个解法较复杂不易理解
+     */
+    public static void lastPrintByLoop(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        TreeNode node = root;
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> markStack = new Stack<>();
+        while(node!=null||!stack.isEmpty()){
+            while(node!=null){
+                stack.push(node);
+                node = node.left;
+            }
+
+            while(!markStack.isEmpty()&&markStack.peek()==stack.peek()){
+                markStack.pop();
+                System.out.print(stack.pop().val + " | ");
+            }
+            if(!stack.isEmpty()){
+                node = stack.peek();
+                markStack.push(node);
+                node = node.right;
+            }
+        }
+    }
+
+    /**
      * 按层遍历二叉树
      */
     public static void levelPrintOutTree(TreeNode root) {
@@ -165,7 +228,7 @@ public class TreeOperation {
                 if (i.right != null) {
                     queue.add(i.right);
                 }
-                System.out.print(i.val);
+                System.out.print(i.val + " | ");
                 queue.poll();
                 i = queue.peek();
             }
@@ -173,8 +236,22 @@ public class TreeOperation {
     }
 
     public static void main(String[] args) {
-        int[] a = {3, 2, 5, 8, 4, 7, 6, 9, 1};
-        TreeNode root = createWholeTree(a);
+        int[] a = {3, 2, 1, 8, 4, 7, 6, 9, 0, 5};
+        TreeNode root = createSearchTree(a);
+        frontPrintOutTree(root);
+        System.out.println("\n----------------");
+        frontPrintByLoop(root);
+        System.out.println("\n----------------");
+        midPrintOutTree(root);
+        System.out.println("\n----------------");
         midPrintByLoop(root);
+        System.out.println("\n----------------");
+        lastPrintOutTree(root);
+        System.out.println("\n----------------");
+        lastPrintByOtherStack(root);
+        System.out.println("\n----------------");
+        lastPrintByLoop(root);
+        System.out.println("\n----------------");
+        levelPrintOutTree(root);
     }
 }
