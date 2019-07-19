a3165065976d4accb570bf2a1066c1334391fa39
Fatezhang <762344938@qq.com>
2019-07-19 19:28:20 +0800

    leetcode 交替打印奇偶数

---
+++ src/main/java/Alogrithm/Alogrithm/PrintZeroEvenOdd/ZeroEvenOdd.java
@@ -0,0 +1,25 @@
+package Alogrithm.Alogrithm.PrintZeroEvenOdd;
+
+import java.util.function.IntConsumer;
+
+/** @Author zhangjiaheng @Description https://leetcode-cn.com/problems/print-zero-even-odd */
+public class ZeroEvenOdd {
+    private int n;
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+
+    }
+}
